f39466abf01be03c5cf5f125a0c7343e7c7db646
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 17:21:04 -0600

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
 
-with open( "style.css" ) as css:
+with open("style/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
     
 st.title(":blue[App Azul - ¡Bienvenido!]")
