14302c5cfabea3a2e6b184850f1d9d3d30e0e681
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 13:58:15 -0600

    Create home.py

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+import datetime
+
+st.set_page_config(page_title="App Azul", 
+                   page_icon="🍣", 
+                   layout="wide")
+
+st.title(":blue[App Azul - ¡Bienvenido!]")
+st.sidebar.success("Selecciona una opción:")
