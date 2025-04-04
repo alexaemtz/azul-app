782091e2496311d7e435aed526744d650ab7e415
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 17:22:00 -0600

    Update Registro.py

--- pages/Registro.py
+++ pages/Registro.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import pytz
 
 st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
+with open("style/style.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 st.title(":blue[App Azul - ¡Bienvenido!]")
 st.header("¡Bienvenido a su App Azul!", divider="rainbow")
 st.write("Esta aplicación le será de ayuda para el monitoreo y control de su diabetes.")
