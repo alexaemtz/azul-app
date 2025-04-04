05438ea3266b16044ca463be4aad75dd5e0a26f7
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 17:16:55 -0600

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
+st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
+
 with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
-st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
+    
 st.title(":blue[App Azul - ¡Bienvenido!]")
 st.header("¡Bienvenido a su App Azul!", divider="rainbow")
 st.write("Esta aplicación le será de ayuda para el monitoreo y control de su diabetes.")
