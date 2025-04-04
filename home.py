bbff72e9bb93940b2e8c3f4fc3988d153701fd18
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 16:42:54 -0600

    Update home.py

--- home.py
+++ home.py
@@ -1,20 +1,5 @@
 import streamlit as st
 
-# --- PAGE SETUP ---
-project_1_page = st.Page(
-  page = "pages/regisro.py",
-  title = "Registro",
-  icon = ":material/description:",
-  default = True,
-)
-
-project_2_page = st.Page(
-  page = "pages/regisro.py",
-  title = "Detección de calorías",
-  icon = ":material/photo_camera:",
-  default = True,
-)
-
 st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
 st.title(":blue[App Azul - ¡Bienvenido!]")
 st.header("¡Bienvenido a su App Azul!", divider="rainbow")
