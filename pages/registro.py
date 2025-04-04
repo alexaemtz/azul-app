f997e42f35aeac752076419ac4aec9258b45062c
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-04 16:37:47 -0600

    Update registro.py

--- pages/registro.py
+++ pages/registro.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import datetime
+from zoneinfo import ZoneInfo
 
 st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
 st.title(":blue[App Azul - ¡Bienvenido!]")
@@ -10,8 +11,11 @@ st.write("Esta aplicación le será de ayuda para el monitoreo y control de su d
 st.subheader("Registro")
 st.write("Cree el registro de glucosa.")
 
-fecha =st.date_input("Fecha:", datetime.datetime.now())
-hora = st.time_input("Hora:", datetime.datetime.now())
+zona_local = ZoneInfo("America/Mexico_City")
+hora_local = datetime.now(tz=zona_local)
+
+fecha =st.date_input("Fecha:",hora_local)
+hora = st.time_input("Hora:",hora_local)
 
 tiempo_medicion = st.text_input("Tiempo de la medición:",
                                 placeholder="Ejemplo: 10 minutos antes de la comida.")
