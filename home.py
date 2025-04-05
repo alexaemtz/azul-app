2a198549224af718a836eaee474b895bd22a0fc0
Alexa Escalante <159209041+alexaemtz@users.noreply.github.com>
2025-04-05 10:11:21 -0600

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,30 @@
 import streamlit as st
 
-st.set_page_config(page_title="App Azul", page_icon="🍣", layout="wide")
-
 with open("style/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
     
 st.title(":blue[App Azul - ¡Bienvenido!]")
 st.header("¡Bienvenido a su App Azul!", divider="rainbow")
 st.write("Esta aplicación le será de ayuda para el monitoreo y control de su diabetes.")
+
+# --- PAGE SETUP ---
+main_page = st.Page("main-page.py",
+                    title="Inicio",
+                    icon=":material/home:"),
+
+register_page = st.Page("views/register.py", 
+                        title="Registro", 
+                        icon=":material/description:"),
+
+counter_page = st.Page("views/detection.py", 
+                       title="Calculo de calorias", 
+                       icon=":material/favorite:"),
+
+# --- NAVIGATION SETUP [WITH SECTIONS] ----
+pg = st.navigation({
+    "Inicio" : main_page,
+    "Registro" : register_page,
+    "Monitoreo" : counter_page
+})
+
+pg.run()
